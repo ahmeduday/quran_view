e479fe354a55456b0db18fc588c83470b9cc245c
janabisoft <janabisoft@hotmail.com>
2022-10-07 00:58:55 +0400

    ...

--- src/routes/info/+page.svelte
+++ src/routes/info/+page.svelte
@@ -1,18 +1,19 @@
-<div>
-    <h1>Info</h1>
-
-    <ul>
-        {#each list as item}
-            <li>
-                <h4>{item.Key}</h4><span>{item.value}</span>
-            </li>
-        {/each}
-    </ul>
-</div>
-
 <script>
-    import { each } from 'svelte/internal';
-import json from './info.json';
+	import { each } from 'svelte/internal';
+	import json from './info.json';
 
-    let list = json;
+	let list = json;
 </script>
+
+<div>
+	<h1>Info</h1>
+
+	<ul style="list-style-type: '--';">
+		{#each list as item}
+			<li style="display: flex;">
+				<h4>{item.Key}</h4>
+				<span>{item.value}</span>
+			</li>
+		{/each}
+	</ul>
+</div>
---
+++ src/routes/soras/+page.svelte
@@ -0,0 +1,26 @@
+<script>
+	import { each } from 'svelte/internal';
+	import json from '../../data/chapters.json';
+    import {Book, PlayFill, InfoCircleFill} from "svelte-bootstrap-icons";
+
+</script>
+
+<div>
+	<h1>Chapters</h1>
+
+	<ul style="list-style-type: none;">
+		{#each json as item}
+			<li >
+                <div style="display: flex; margin:20px;">
+                    <span>{item.key}. </span>
+                    <span>{item.value}</span>
+                    <span style="margin-right: 100px;"></span>
+                    <Book style="margin-right:20px;"/>
+                    <PlayFill color="red" style="margin-right:20px;"/>
+                    <InfoCircleFill color="blue" style="margin-right:20px;"/>
+                </div>
+                <hr>
+			</li>
+		{/each}
+	</ul>
+</div>
